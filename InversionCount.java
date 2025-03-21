54528c37c93af091f12793cefc000f301e080c60
Amany2602 <93898385+Amany2602@users.noreply.github.com>
2025-03-21 16:45:43 +0545

    Update InversionCount.java

--- InversionCount.java
+++ InversionCount.java
@@ -1,3 +1,24 @@
-git add InversionCount.java
-git commit -m "Add inversion count program"
-git push origin main
+public class InversionCount {
+
+    // Function to count inversions in the array
+    public static int countInversions(int[] array) {
+        int invCount = 0;
+        int n = array.length;
+        
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+              
+                if (array[i] > array[j]) {
+                    invCount++;
+                }
+            }
+        }
+        
+        return invCount;
+    }
+
+    public static void main(String[] args) {
+        int[] array = { 5, 3, 2, 4, 1 };
+        System.out.println("Total Inversions: " + countInversions(array));
+    }
+}
